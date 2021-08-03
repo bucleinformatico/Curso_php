c1a62ea73da1907fed4d23e9616dfd47f6c326e2
Alejandro López Sanmartino <bucleinformatico@gmail.com>
2021-08-03 21:03:45 +0200

    03/08/2021

---
+++ Formularios/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+<form action="recibe.php" method="post">; //metodo post podemos enviar archivos con get no
+<input type="text" placeholder="Nombre"  name="nombre">
+<br/>
+<label for="Hombre" Hombre></label>
+<input type="radio"  name="sexo" value="Hombre">
+
+<select name="year" id="">
+<option value ="2000"> 2000</option>
+<option value ="2030"> 2030</option>
+<option value ="2040"> 2040</option>
+<option value ="2049"> 2049</option>
+</select>
+
+
+<label for="terminos"> Aceptar condiciones </label>
+<input type="checbox" name= "terminos" id="terminos" value="ok">
+<input type="submit" Value="Enviar">
+    </form>
+</head>
+<body>
+    
+</body>
+</html>
---
+++ Formularios/recibe.php
@@ -0,0 +1,18 @@
+<?php
+/*
+if ($_POST){
+
+    $nombre= $_POST['nombre'];
+
+    echo 'Hola, ' . $nombre .' eres especial';
+} else{
+
+header ('Location::http//:http://localhost/Formularios/');
+}
+
+ */
+
+ print_r ($_GET);
+
+
+?>
